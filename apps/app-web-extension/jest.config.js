dbda34a24a96d671c656c9d30f0a5f07e92962cd
Richard Sithole <richardsit@gmail.com>
2022-01-15 18:59:19 +0100

    rename app

--- apps/app-web-extension/jest.config.js
+++ apps/app-web-extension/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'chrome-extension',
+  displayName: 'app-web-extension',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -8,7 +8,7 @@ module.exports = {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/apps/chrome-extension',
+  coverageDirectory: '../../coverage/apps/app-web-extension',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
